635e9f3ecf835d2e4f472b8cc4e6b24b4f3fa2cb
RinggoGalihSadewo <ringgo.118140076@student.itera.ac.id>
2021-06-26 11:37:09 +0700

    update

--- resources/views/admin/detail.blade.php
+++ resources/views/admin/detail.blade.php
@@ -5,21 +5,6 @@
 
 @section('container')
 
-<!-- <ul class="list-group list-group-flush">
-  <li class="list-group-item" style="background-color: #4366a3; color: white;"><h4>Detail Data Perizinan Trayek Transportasi Darat</h4></li>
-  <li class="list-group-item"><b>Nama Pribadi :</b> {{ $client->namaPribadi }}</li>
-  <li class="list-group-item"><b>Alamat:</b> {{ $client->alamat }}</li>
-  <li class="list-group-item"><b>TTL:</b> {{ $client->ttl }}</li>
-  <li class="list-group-item"><b>Nama Perusahaan:</b> {{ $client->namaPerusahaan }}</li>
-  <li class="list-group-item"><b>Trayek:</b> {{ $client->trayek }}</li>
-  <li class="list-group-item"><b>Jumlah Armada:</b> {{ $client->jmlhArmada }}</li>
-  <li class="list-group-item"><b>Plat Kendaraan:</b> {{ $client->platKendaraan }}</li>
-  <li class="list-group-item"><b>Merk Kendaraan:</b> {{ $client->merk }}</li>
-  <li class="list-group-item"><b>Warna:</b> {{ $client->warna }}</li>
-  <li class="list-group-item"><b>Bahan Bakar:</b> {{ $client->bahanBakar }}</li>    
-</ul> -->
-
-
 	<table class="table table-bordered">
 	  <thead style="background-color: #4366A3;">
 	    <tr class="text-white">
@@ -30,7 +15,7 @@
 
 	    <tr>
 	        <td>
-	        	<b>Nama Pribadi :</b> {{ $client->namaPribadi }}
+	        	<b>Nama Pribadi:</b> {{ $client->namaPribadi }}
 	        </td>
 	    </tr>
 
