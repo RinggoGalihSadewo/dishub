69b81d3a57970a7967588c0f766ad8c4e6c7cf28
vinaoktariana118140061 <vina.118140061@student.itera.ac.id>
2021-06-28 14:04:15 +0700

    halaman petunjuk pengisian form

--- resources/views/guide/index.blade.php
+++ resources/views/guide/index.blade.php
@@ -7,7 +7,11 @@
 @section('container')
 
 <div class="guide">		
-	<img src="/img/guide.jpeg" width="835px" height="591px">
+	<!-- <img src="/img/guide.jpeg" width="835px" height="591px"> -->
+	<img src="/img/langkah1.png" width="400px" height="700px">
+	<img src="/img/langkah2.png" width="400px" height="700px">
+	<img src="/img/langkah3.png" width="400px" height="700px">
+	<img src="/img/langkah4.png" width="400px" height="700px">
 </div>
 
 @endsection
