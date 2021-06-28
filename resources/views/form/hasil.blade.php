b3c3b7845d2feeafa173d291c091e8fd1833c95d
RinggoGalihSadewo <ringgo.118140076@student.itera.ac.id>
2021-06-28 18:40:48 +0700

    embeds

--- resources/views/form/hasil.blade.php
+++ resources/views/form/hasil.blade.php
@@ -66,40 +66,18 @@
 			<b>(Materai 6000)</b>
 		</div>
 	</div>
-		    
-<!-- 	<center>
+		    	
+	<br><br>
 
-	<h3>Hasil Form</h3>
+<center>
 
-	<hr>
+	<h4>Alamat GPS Dinas Perhubungan Provinsi Lampung: </h4>
 
-	<p><b>Nama Pribadi :</b> {{ $request->nama }}</p>
-		        	 
-	<p><b>Alamat Perusahaan:</b> {{ $request->alamat }}</p>
-		    	
-	<p><b>TTL:</b> {{ $request->ttl }}</p>
-		    		    	
-	<p><b>Nama Perusahaan:</b> {{ $request->perusahaan }}</p>
-		    	
-	<p><b>Trayek:</b> {{ $request->trayek }}</p>
-		    	
-	<p><b>Jumlah Armada:</b> {{ $request->jmlArmada }}</p>
-		    	
-	<p><b>Plat Kendaraan:</b> {{ $request->plat }}</p>
-		    	
-	<p><b>Merk Kendaraan:</b> {{ $request->merk }}</p>
-		    	
-	<p><b>Warna Kendaraan:</b> {{ $request->warna }}</p>
-		    	
-	<p><b>Bahan Bakar:</b> {{ $request->bahanBakar }}</p>
-
-
-	<div class="embed-responsive embed-responsive-4by3" style="width: 200px; height: 200px">
+	<div class="embed-responsive embed-responsive-16by9" style="width: 65%; height: 400px">
 
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.8874315927283!2d105.25341381448622!3d-5.434064155742303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40da30bd702e13%3A0x8a05d5fc73bcbbaa!2sLampung%20Provincial%20Transport%20Department!5e0!3m2!1sen!2sid!4v1624432836269!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
-  
-</div> -->
+	</div> 
 
 
 </center>	    	
